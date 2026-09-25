8f9a1d4e2d7a9c6020d24a933dad310916e72c90
kashimontree-tech <kashimontree@gmail.com>
2026-09-25 22:23:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -846,7 +846,7 @@ async function rememberActor(event, actor) {
 
 }
 
-  try {async function rememberMessage(event, actor, content, messageType) {
+  async function rememberMessage(event, actor, content, messageType) {
 
   if (!supabaseReady()) return;
 
